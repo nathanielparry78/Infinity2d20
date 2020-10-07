725585cf0e78f3d2ea36f081e1b38aa3de3c773a
Christoffer Lundberg <lundberg.christoffer@outlook.com>
2020-10-07 22:34:32 +0200

    Better format for news.

--- WebTools/src/news.ts
+++ WebTools/src/news.ts
@@ -1,13 +1,13 @@
 export class News {
     static getWhatsNew() {
-        return "<b>WHAT'S NEW?</b><br/><br/><div class='news'>"+
-            "- Fixed a bug that caused Antipode Tribe to always become Bitter Leaves.<br/>"+
-            "- Fixed a bug that caused the Medical Experiment career event to sometimes give you an undefined attribute.<br/>"+
-            "- Fixed a bug that prevented progress when rolling/selecting the Haqqislam 'ideals' career event.<br/>"+
-            "- Fixed a bug that prevented progress when rolling/selecting the PanOceania 'cybernetic limb' adolescence event.<br/>"+
-            "- Fixed a bug that prevented progress when selecting certain Uplift Host Types.<br/>"+
-            "- Fixed a bug that caused the application to freeze when selecting a Youth Event as a Nomad character.<br/>"+
-            "- Fixed a bug that caused the final page to not appear appropriately.<br/>"+
-            "</div>";
+        return "<b>WHAT'S NEW?</b><br/><br/><div class='news'><ul>"+
+            "<li>Fixed a bug that caused Antipode Tribe to always become Bitter Leaves.</li>"+
+            "<li>Fixed a bug that caused the Medical Experiment career event to sometimes give you an undefined attribute.</li>"+
+            "<li>Fixed a bug that prevented progress when rolling/selecting the Haqqislam 'ideals' career event.</li>"+
+            "<li>Fixed a bug that prevented progress when rolling/selecting the PanOceania 'cybernetic limb' adolescence event.</li>"+
+            "<li>Fixed a bug that prevented progress when selecting certain Uplift Host Types.</li>"+
+            "<li>Fixed a bug that caused the application to freeze when selecting a Youth Event as a Nomad character.</li>"+
+            "<li>Fixed a bug that caused the final page to not appear appropriately.</li>"+
+            "</ul></div>";
     }
 }
